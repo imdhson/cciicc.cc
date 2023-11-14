f44f329fe98afd5d0ae08b19151d8a72cb2ca31a
imdhson <hhammer1234@gmail.com>
2023-11-14 11:42:25 +0900

    add test.html

--- service/GetSpaceFrom_space_id.go
+++ service/GetSpaceFrom_space_id.go
@@ -1,15 +1,21 @@
 package service
 
-import "ub/types"
+import (
+	"time"
+	"ub/types"
+)
 
 func GetSpaceFrom_space_id(space_id string) (*types.Space, bool) {
 	spaces := types.GetInstance_spaces()
 	var rst *types.Space
 	rst_success := false
-	for _, v := range *spaces {
-		if space_id == v.Sp_id {
+	for i, _ := range *spaces {
+		if space_id == (*spaces)[i].Sp_id {
 			rst_success = true
-			rst = &v
+			rst = &(*spaces)[i]
+			(*spaces)[i].Sp_view += 1
+			(*spaces)[i].Sp_lastupdate = time.Now()
+			return rst, rst_success
 		}
 	}
 	return rst, rst_success

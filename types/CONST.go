666d1216489128e1401222775b5d05f9cf3421b6
imdhson <hhammer1234@gmail.com>
2023-11-24 11:30:20 +0900

    const change

--- types/CONST.go
+++ types/CONST.go
@@ -21,9 +21,9 @@ const (
 )
 
 const ( //KR
-	SERVICE_NAME   string = "usefulBoard"
-	SERVICE_DETAIL string = "브레인스토밍을 쉽게"
-	FOOTER_TERMS   string = "서비스 이용시 약관에 동의하고 쿠키 사용에 동의하는 것으로 간주됨."
+	SERVICE_NAME   string = "cciicc"
+	SERVICE_DETAIL string = "주제나 아이디어를 쉽게 공유할 수 있어요. 교육현장이나 사내 회의 등 환경에서 도움이 될거예요."
+	FOOTER_TERMS   string = "서비스 이용시 약관과 쿠키 사용에 동의하는 것임"
 
 	MAIN_NEXT_UP string = "이어서 시작하기"
 	MAIN_HOST    string = "새로운 space 생성하기"
@@ -31,14 +31,14 @@ const ( //KR
 
 	HOST_DETAIL          string = "Host로 시작하기"
 	HOST_SPACENAME       string = "Space 이름"
-	HOST_SPACENAME_INPUT string = "Space 이름을 지정하세요. 예) 창의문제해결 아이디어"
+	HOST_SPACENAME_INPUT string = "Space 이름을 지정하세요. 예)회의실"
 	HOST_USERNAME        string = "당신의 닉네임"
 	HOST_USERNAME_INPUT  string = "당신의 닉네임을 입력하세요"
 	HOST_FORM_BUTTON     string = "space 생성하기"
 
 	GUEST_DETAIL         string = "Guest로 시작하기"
 	GUEST_SPACEID        string = "Space ID 입력"
-	GUEST_SPACEID_INPUT  string = "Host가 제공한 Space ID를 입력하세요. 예) "
+	GUEST_SPACEID_INPUT  string = "Host가 제공한 Space ID를 입력하세요. 예)" //ex) 이후에 임의의 코드가 나옴
 	GUEST_USERNAME       string = "당신의 닉네임"
 	GUEST_USERNAME_INPUT string = "당신의 닉네임을 입력하세요"
 	GUEST_FORM_BUTTON    string = "space 입장하기"
